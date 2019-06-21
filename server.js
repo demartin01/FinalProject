84809f4dc04b3ec36ee3e716d2532abf5510ece8
denise martin <denise.martin@vanderbilt.edu>
2019-06-21 12:37:49 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+//server
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
+// middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+};
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb:// localhost:27017/homesdb", {useNewUrlParser: true});
+
+app.listen(PORT, () => console.log(`now listening on http://localhost:${PORT}`))
+
+
+
+
