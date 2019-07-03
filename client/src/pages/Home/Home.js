eafcf80f6aad060562702fcbc305e7cb76d391e1
denise martin <denise.martin@vanderbilt.edu>
2019-07-02 19:26:39 -0500

    changes to images

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => (
   <div className="homeDiv">
     <div className="container">
       <h1 className="header">StayorGo</h1>
-      <p>StayorGo is designed to help simply the decision making process. StayorGo is Moving app that helps make decisions as to whether or not you should move or stay. If you decide to move where does your money go farther.Nashville is booming if I sell my house and obtain cash equity. Where can I have an improved my quality of living or should I just stay here. 
+      <p>StayorGo is designed to help simply the decision making process. StayorGo is Moving app that helps make decisions as to whether or not you should move or stay. If you decide to move where does your money go farther. Nashville is booming if I sell my house and obtain cash equity. Where can I have an improved quality of living or should I just stay at my current home. 
 
         {/* <PropertyTable /> */}
       </p>
@@ -15,7 +15,7 @@ const Home = () => (
 
     <div className="section white">
       <div className="container">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, corrupti quo repellendus alias deleniti nostrum accusamus nihil a! Non eos, quidem sequi necessitatibus eius nobis, excepturi unde repellendus quisquam odit aut dolorum ut assumenda? Fuga sint quae saepe cum quas minima aut numquam. Illum, recusandae beatae assumenda velit voluptas alias?</p>
+        <p>Sign-in to create your personalized searches using Zillow.</p>
       </div>
     </div>
     <a href="/snow"><div className="bottom-image"></div></a>
--- client/src/pages/Snow/Snow.js
+++ client/src/pages/Snow/Snow.js
@@ -4,7 +4,7 @@ import "./Snow.css";
 const Snow = () => (
   <div className="snowDiv">
       <div className="container">
-        <h1 className="header">Snowy Mountain</h1>
+        <h1 className="header">Your Favorites</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, corrupti quo repellendus alias deleniti nostrum accusamus nihil a! Non eos, quidem sequi necessitatibus eius nobis, excepturi unde repellendus quisquam odit aut dolorum ut assumenda? Fuga sint quae saepe cum quas minima aut numquam. Illum, recusandae beatae assumenda velit voluptas alias?</p>
       </div>
   </div>
